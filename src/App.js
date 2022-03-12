0d6b057827d9cf3797a470830fd875141745d377
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-03-12 23:22:28 +0900

    added state got movies.json
    영화에 대한 데이터 JSON을 다운로드

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      Hello!
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movie: []
+  };
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading...": "we are ready"}</div>
+  }
 }
 
 export default App;

137262030a59c6f1e59563021e37fbb2446c956b
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-03-20 23:12:31 +0900

    Navigation 생성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./routes/Home"
 import About from "./routes/About"
+import Detail from "./routes/Detail"
+import Navigation from "./components/Navigation";
 
 function App() {
   return <BrowserRouter>
+    <Navigation />
     <Routes>
       <Route path="/" exact={true} element={<Home />} />
+      <Route path="/react_movie_recommender" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/movie-detail" element={<Detail />} />
     </Routes>
   </BrowserRouter>
 }

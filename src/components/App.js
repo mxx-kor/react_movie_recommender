34f78d17bdaa152e9219091afe5b8c5b236dda31
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-05-04 17:10:21 +0900

    settings

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 function App() {
-  return <div>
-    확인
-  </div>
+  return <Router>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/:id" element={<Detail />} />
+    </Routes>
+  </Router>
 }
 
 export default App;

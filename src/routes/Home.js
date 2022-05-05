83f8d57942de53bebdeac226776764e1ec0e89aa
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-05-05 18:34:32 +0900

    used graphQL and get API

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,72 @@
 import React from "react";
-export default () => <h1>Home</h1>;
+import { useQuery, gql } from "@apollo/client";
+import styled from 'styled-components'
+import Movie from "../components/Movie";
+
+const GET_MOVIES = gql`
+    {
+        movies{
+            id
+            medium_cover_image
+        }
+    }
+`
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+// const Movies = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(4, 1fr);
+//   grid-gap: 25px;
+//   width: 60%;
+//   position: relative;
+//   top: -50px;
+// `;
+
+
+export default () => {
+    const { loading, data } = useQuery(GET_MOVIES);
+    console.log(loading, data)
+    return (
+        <Container>
+            <Header>
+                <Title>Apollo 2020</Title>
+                <Subtitle>I love GraphQL</Subtitle>
+            </Header>
+            {loading && <Loading>Loading...</Loading>}
+            {!loading && data.movies && data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+        </Container>
+    );
+};
